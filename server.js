3a50f9bb1cbfa8257b3f6ac2af92fbaf43049b51
Gayatri Sarnobat <gayatrisarnobat@Gayatris-MacBook-Air.local>
2023-11-10 18:41:36 +0530

    feat: handling disconnect, resetting the game

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ io.on('connection', (socket) => {
 
     ++readyPlayerCount;
 
-    if (readyPlayerCount === 2) {
+    if (readyPlayerCount % 2 === 0) {
       io.emit('startGame', socket.id);
     }
   });
@@ -29,6 +29,10 @@ io.on('connection', (socket) => {
 
   socket.on('ballMove', (ballData) => {
     socket.broadcast.emit('ballMove', ballData);
+  });
+
+  socket.on('disconnect', (reason) => {
+    console.log(`Client ${socket.id} disconnected: ${reason}`);
   });
 });
 
