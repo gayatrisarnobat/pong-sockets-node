ebf53721a4873c755894629b52d6424279b39c87
Gayatri Sarnobat <gayatrisarnobat@Gayatris-MacBook-Air.local>
2023-11-10 18:34:24 +0530

    feat: multiplayer pong

---
+++ server.js
@@ -0,0 +1,37 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const PORT = 3000;
+
+let readyPlayerCount = 0;
+
+io.on('connection', (socket) => {
+  console.log(`A user connected: ${socket.id}`);
+
+  socket.on('ready', () => {
+    console.log('Player ready', socket.id);
+
+    ++readyPlayerCount;
+
+    if (readyPlayerCount === 2) {
+      io.emit('startGame', socket.id);
+    }
+  });
+
+  socket.on('paddleMove', (paddleData) => {
+    socket.broadcast.emit('paddleMove', paddleData);
+  });
+
+  socket.on('ballMove', (ballData) => {
+    socket.broadcast.emit('ballMove', ballData);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
